8d341c4aebb0df2987761b9acdefcaecbb659623
Tim Weber <scy@scytale.name>
2009-10-05 18:05:13 +0200

    Add frqbt.core.Tracker and test its version.

--- core/test.py
+++ core/test.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
+import frqbt.core
+import re
 import unittest
+
+class TrackerTests(unittest.TestCase):
+	
+	def setUp(self):
+		self.instance = frqbt.Core.Tracker()
+	
+	def testVersion(self):
+		self.assertTrue(re.match('^0\.0-git$', self.instance.version),
+			'Tracker.version is strange.')
 
 # If we have been called directly (not imported), run the test.
 if __name__ == '__main__':
---
+++ frqbt/core.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+class Tracker:
+	version = '0.0-git'
