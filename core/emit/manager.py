179709705aa8dbfa8f5c03f95498974fdbf32d81
Tim Weber <scy@scytale.name>
2009-10-05 20:11:57 +0200

    Some stubs for the Issue class.

--- core/emit/manager.py
+++ core/emit/manager.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 
+class Issue:
+	def save(self):
+		pass
+
 class Tracker:
 	version = '0.0-git'
+	
+	def createIssue(self):
+		return Issue()
--- core/test.py
+++ core/test.py
@@ -5,13 +5,19 @@ import re
 import unittest
 
 class TrackerTests(unittest.TestCase):
-	
 	def setUp(self):
 		self.instance = emit.manager.Tracker()
 	
 	def testVersion(self):
 		self.assertTrue(re.match('^0\.0-git$', self.instance.version),
 			'Tracker.version is strange.')
+
+class CreateTests(unittest.TestCase):
+	def setUp(self):
+		self.instance = emit.manager.Tracker()
+	
+	def testCreate(self):
+		self.assertTrue(isinstance(self.instance.createIssue(), emit.manager.Issue))
 
 # If we have been called directly (not imported), run the test.
 if __name__ == '__main__':
