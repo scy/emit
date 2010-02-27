af9f8e8f92b66f6b1f24c5aebdf3ca25585b1ccf
Michael Stapelberg <michael@stapelberg.de>
2010-02-27 03:24:06 +0100

    Implement submitting new bug reports to the EMIT core

--- frontends/web/App-emit-Web/lib/App/emit/New.pm
+++ frontends/web/App-emit-Web/lib/App/emit/New.pm
@@ -22,8 +22,31 @@ sub new_issue {
     my $self = shift;
     my $emit = emit->new;
     my $tmpl = $self->load_tmpl('new.tmpl');
-
     my @fields = @{$emit->fields};
+
+    if ($self->query->param('submit')) {
+        # Collect the data which the user sent
+        my $query = $self->query;
+
+        # Only keep the name of each field
+        @fields = map { $_->{name} } @fields;
+
+        # Strip out the fields which were not submitted
+        @fields = grep { $query->param($_) } @fields;
+
+        # Generate a hash out of the submitted fields
+        my %submitted = map { ($_, $query->param($_)) } @fields;
+
+        my ($status, $msg) = $emit->create_bug(\%submitted);
+        # TODO: templates for the status / redirect to the bug detail page
+        if ($status) {
+            return 'Bug successfully created: ' . $msg;
+        } else {
+            return 'Error: ' . $msg;
+        }
+    } else {
+        return '<pre>'.Dumper($self);
+
     apply { $_->{type} = 'field_' . $_->{type} . '.tmpl' } @fields;
 
     # TODO: user’s $LANG. set name to display, strip other name-<LANG> fields
@@ -32,6 +55,7 @@ sub new_issue {
     $tmpl->param(FIELDS => \@fields);
 
     return $tmpl->output;
+    }
 }
 
 1
--- frontends/web/App-emit-Web/lib/emit.pm
+++ frontends/web/App-emit-Web/lib/emit.pm
@@ -2,12 +2,16 @@ package emit;
 # vim:ts=4:sw=4:expandtab
 
 use Moose;
+use JSON::XS;
+use AnyEvent::HTTP;
+use Try::Tiny;
 
 use Data::Dumper;
 
 has 'version' => (is => 'ro', isa => 'Str', default => 'foo');
 
-method fields($current_fields?) {
+sub fields {
+    my ($self) = @_;
     # TODO: correctly format the fields if given
     # TODO: correctly receive the fields via HTTP and decode the JSON
 
@@ -21,6 +25,42 @@ method fields($current_fields?) {
         { name => 'distro', type => 'line', 'name-en' => 'Distribution / flavor' },
         { name => 'notfinal', type => 'checkbox', 'name-en' => 'I recognize this software is not final' },
     ];
+}
+
+=head2 create_bug($fields)
+
+Submits this create bug request to EMIT.
+Returns a tuple of either:
+(0, $internal_error_message)
+(0, $emit_error_message)
+(1, $document_id)
+
+=cut
+sub create_bug {
+    my ($self, $bug) = @_;
+
+    my $json = encode_json $bug;
+    my $cv = AnyEvent->condvar;
+    http_post 'http://localhost:81/write', $json, sub { $cv->send(@_) };
+    my ($body, $headers) = $cv->recv;
+
+    if ($headers->{Status} =~ /^59/) {
+        # Internal error (could not resolve host, …)
+        return (0, $headers->{Reason});
+    }
+
+    unless ($headers->{Status} =~ /^2/) {
+        # EMIT error, just pass on the message
+        return (0, $body);
+    }
+
+    # everything went well, try to decode the body’s JSON and return _id
+    my $res;
+    try { $res = decode_json $body };
+    return (0, 'Invalid response from EMIT core') unless defined($res->{_id});
+
+    return (1, $res->{_id});
+
 }
 
 1
